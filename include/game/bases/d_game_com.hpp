bb5af1c35c9f1a350c2d62adeb9d4b3547b9c620
RootCubed <liam@rootcubed.dev>
2024-12-23 19:26:08 +0100

    Decompile dCourseSelectGuide_c (matching; not cleaned up)

--- include/game/bases/d_game_com.hpp
+++ include/game/bases/d_game_com.hpp
@@ -56,8 +56,12 @@ namespace dGameCom {
     void getGlbPosToLyt(mVec3_c &);
     bool someCheck(mVec3_c *a, AreaBound *b);
     void CreateSmallScore(const mVec3_c &, int, int, bool);
+    u8 GetAspectRatio();
+    bool PlayerEnterCheck(int);
 
     /// @unofficial
     void updateSelectCursor(nw4r::lyt::Picture *pic, int index, bool useSpecialDraw);
     float getDispCenterY();
+
+    void DispSizeScale(nw4r::math::VEC2 &scale);
 }
